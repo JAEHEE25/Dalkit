ffce0a2fc2c7a0b20070138a8da21cebf821f717
JAEHEEE <y2hjjh@naver.com>
2022-11-15 00:56:02 +0900

    Initial commit

---
+++ src/main/java/dalkit/dalkit/DalkitApplication.java
@@ -0,0 +1,13 @@
+package dalkit.dalkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalkitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DalkitApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dalkit/dalkit/DalkitApplicationTests.java
@@ -0,0 +1,13 @@
+package dalkit.dalkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalkitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
